e87e15a5aa9ef7b6f25d27839fe9d34ff830713b
david <david@legenddigital.com.au>
2022-04-20 22:59:51 +0800

    note

--- protobuf/build.sh
+++ protobuf/build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 cd `dirname $0`
 
-#--go_out : 指定输出go代码
+#--go_out : 指定输出go代码， --go_out的写法是参数之间用逗号隔开，最后加上冒号来指定代码生成的位置
 #plugins=grpc : 为protoc-gen-go插件的工作方式 .proto文件中的service是grpc的扩展功能，需要使用grpc插件进行解析才能生成对应的接口定义代码。
-#./pb为gout_out 的第二个参数，为go代码生成的位置
-protoc  --go_out=plugins=grpc:./pb ./foo.proto
+#paths=import : paths参数有两个选项，import 和 source_relative
+#默认为import，表示按照生成go代码的包的全路径去创建目录层级，source_relative表示按照proto源文件目录层级去创建go代码目录层级
+#例：
+#如果foo.proto 文件中，参数为 option go_package = "./pb;pb";
+#设置paths=import 代码生成在 ./protobuf/pb， 而设置 paths=source_relative 则，代码生成在./protobuf文件夹下
+protoc  --go_out=plugins=grpc,paths=import:./ ./foo.proto
